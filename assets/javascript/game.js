04e30ab56722a8e29dd3c320a0e947cb869c6f12
Majid <tomajid@yahoo.com>
2017-09-16 02:35:53 -0700

    Updated Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -52,88 +52,182 @@ var stateCapitals = [
        ]
 
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
-			   "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+			   "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"," "];
 
 
-var random = Math.floor((Math.random()*(stateCapitals.length-1))); 
-var state = stateCapitals[random][0]; 
-var capital = stateCapitals[random][1];
-var capitalName = capital.split("");
-var guessedStr = capital.split("");
-var imgIndex=0;
+var initialized = false;
+var capitalName;
+var guessedStr;
+var lossIndex=0;
 var guessedRight = false;
-
-console.log("*****capitalName is " + capitalName);
-
+var userWins = false;
+var wins=0;
+var losses=0;
+var repeatChars="0";
+var song = null;
 
 window.onload = init;
 
-function init() {
-	var stateField = document.getElementById("stateNameFld");
-	var randomState = document.createTextNode(state);
-	stateField.appendChild(randomState);
+function init() 
+{
+    initButtons();
+
+    var random = Math.floor((Math.random()*(stateCapitals.length-1)));
+    var state = stateCapitals[random][0]; 
+    var capital = stateCapitals[random][1];
+
+    capitalName = capital.split("");
+    guessedStr = capital.split("");
+    lossIndex=0;
+    guessedRight = false;
+    userWins = false;
+    repeatChars = "0";
+
+    playSong("playMusic");
+
+	document.createTextNode(state);
+    document.getElementById("stateNameFld").innerHTML = state;
 	document.getElementById("stateNameFld").style.color = "red";
 
-	 for (var i=0; i < letters.length; i++)
-     {
-        var letterBtn = $('<buttons>');
-        letterBtn.attr('class', 'letter-button letter letter-button-color');
-        letterBtn.attr('data-letter', letters[i]);
-        letterBtn.text(letters[i]);
-        $("#buttons").append(letterBtn);
-     };
 
 	// every letter in the word is symbolized by an underscore in the guessfield
+
 	for (var i = 0; i < guessedStr.length; i++)
 	{
 		guessedStr[i] = "_ ";
 	};
+
+    var str = "State Capital Name: "
+    str += guessedStr.join("");
+    document.createTextNode(str);
+    document.getElementById("capitalNameFld").innerHTML = str;
+
+    str = "Incorrect Guesses: ";
+    document.createTextNode(str);
+    document.getElementById("wrongLetters").innerHTML = str;
+    document.getElementById("wrongLetters").style.color = "blue";
+
+    refreshFields();
+
+    console.log(capitalName);
+}
+
+function initButtons()
+{
+    if (!initialized)
+    {
+         for (var i=0; i < letters.length; i++)
+         {
+            var letterBtn = $('<buttons>');
+            letterBtn.attr('class', 'letter-button letter letter-button-color');
+            letterBtn.attr('data-letter', letters[i]);
+            letterBtn.text(letters[i]);
+            letterBtn.on("click", processLetter);
+            $("#buttons").append(letterBtn);
+        }
+        initialized = true;
+     }
 }
 
 
-//checks if the the letter provided by the user matches one or more of the letters in the word
-var checkLetter = function(){
-	console.log("*****capitalName is " + capitalName);
-	console.log("3) checkLetter");
-
-	var letter = document.gamePad.elements["guessedLetter"]; 
-	var userGuess = letter.value; // the letter provided by the user
-
-	for (var i = 0; i < capitalName.length; i++){
-		if(capitalName[i].toUpperCase() === userGuess.toUpperCase()){
-			guessedStr[i] = userGuess + " ";
-			guessedRight = true;
-			var capitalFld = document.getElementById("capitalNameFld");
-			var nextChar = document.createTextNode(guessedStr[i]);
-			capitalFld.appendChild(nextChar);
-			console.log("user guess is right");
-		}
-	}
-	
-	// if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
-	if(!guessedRight){
-		var wrongLetters = document.getElementById("wrongLetters");
-		var str = document.createTextNode(" " + userGuess);
-		wrongLetters.appendChild(str); 
-		imgIndex++;
-		var hangman = document.getElementById("hangman");
-    	hangman.src = "assets/images/hangman" + imgIndex + ".png";
-	}
-
-	//checks if all letters have been found
-	var correctGuess = true;
-	for (var i = 0; i < guessedStr.length; i++){
-		if(guessedStr[i] === "_ "){
-			correctGuess = false;
-		}
-	}
-	if(correctGuess){
-			window.alert("You win!");
-	}
-	
-	//once you got six wrong letters, you lose
-	if(imgIndex === 6){
-		window.alert("Uh...I guess you're dead now.");
-	}
+function playSong(id)
+{
+    if(song != null) 
+    {
+        song.pause();
+    }
+    song = document.getElementById(id); 
+    song.play();
 }
 
+function refreshFields()
+{
+    var str = "Remainig Guesses: "
+    str += (6-lossIndex);
+    document.createTextNode(str);
+    document.getElementById("remainigGuesses").innerHTML = str;
+
+    str = "Wins: "
+    str += wins;
+    document.createTextNode(str);
+    document.getElementById("wins").innerHTML = str;
+
+    str = "Losses: "
+    str += losses;
+    document.createTextNode(str);
+    document.getElementById("losses").innerHTML = str;
+
+    var hangman = document.getElementById("hangman");
+    hangman.src = "assets/images/hangman" + lossIndex + ".png";
+}
+
+
+var processLetter = function() {
+
+    if (userWins === true)
+    {
+        return;
+    }
+ 
+    guessedRight = false;
+    var userGuess = $(this).text();
+
+    if (repeatChars.includes(userGuess))
+    {
+        return;
+    }
+
+    repeatChars += userGuess;
+ 
+    //var str = "State Capital Name: "
+    for (var i = 0; i < capitalName.length; i++){
+        if(capitalName[i].toUpperCase() === userGuess.toUpperCase()){
+            guessedStr[i] = userGuess;
+            guessedRight = true;
+            var str = guessedStr.join("");
+            str = "State Capital Name: " + str;
+            document.createTextNode(str);
+            document.getElementById("capitalNameFld").innerHTML = str;
+        }
+    }
+    
+    // if a guessed letter is not in the word, the letter will be put on the 
+    //"wrong letters"-list and hangman grows
+
+    if(guessedRight === false){
+        var wrongLetters = document.getElementById("wrongLetters");
+        var str = document.createTextNode(" " + userGuess);
+        wrongLetters.appendChild(str); 
+        lossIndex++;
+    }
+
+    //checks if all letters have been found
+   
+    userWins = true;
+    for (var i = 0; i < guessedStr.length; i++){
+        if(guessedStr[i] === "_ "){
+            userWins = false;
+        }
+    }
+
+
+    if(userWins === true){
+        playSong("winMusic");
+        wins++;
+        str = "Congratulations, You Won!!!"
+        document.createTextNode(str);
+        document.getElementById("wrongLetters").innerHTML = str;
+        document.getElementById("wrongLetters").style.color = "red";
+    }
+    
+    if(lossIndex === 6){ 
+        playSong("loseMusic");
+        losses++;
+        str = "Sorry, You Lost!!!"
+        document.createTextNode(str);
+        document.getElementById("wrongLetters").innerHTML = str;
+        document.getElementById("wrongLetters").style.color = "red";
+    }
+
+    refreshFields();
+}
